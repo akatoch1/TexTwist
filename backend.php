e2a6896757d899ff2042a64b182caa526be95db4
akatoch1 <44404876+akatoch1@users.noreply.github.com>
2019-09-23 09:06:44 -0400

    Add files via upload

--- backend.php
+++ backend.php
@@ -1,4 +1,6 @@
 <?php
+$myrack;
+//$results;
 function generate_rack($n){
   $tileBag = "AAAAAAAAABBCCDDDDEEEEEEEEEEEEFFGGGHHIIIIIIIIIJKLLLLMMNNNNNNOOOOOOOOPPQRRRRRRSSSSTTTTTTUUUUVVWWXYYZ";
   $rack_letters = substr(str_shuffle($tileBag), 0, $n);
@@ -8,13 +10,12 @@ function generate_rack($n){
   return implode($temp);
 };
 
-
 $myrack = generate_rack(6);
 $racks = [];
 function permutations() {
     for($i = 0; $i < pow(2, strlen($myrack)); $i++){
 	$ans = "";
-	for($j = 0; $j < strlen($myrack); $j++){
+	for($j = 0; $j < strlen($myrack); $j++) {
 		//if the jth digit of i is 1 then include letter
 		if (($i >> $j) % 2) {
 		  $ans .= $myrack[$j];
@@ -27,9 +28,69 @@ function permutations() {
 }
 $racks = array_unique($racks);
 
+$dbhandle  = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
+if (!$dbhandle) die ($error);
+$query = 'SELECT words FROM racks WHERE rack="'. $rack . '"';
+$statement = $dbhandle->prepare($query);
+$statement->execute();
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+    #print_r($results
 
 
-echo json_encode(array("rack"=>$myrack, "subracks"=>$racks));
+
+function isValid($word) {
+	
+	$temp = $word;
+    $temp = str_split($temp);
+    sort($temp);
+    $temp = implode('', $temp);
+	foreach ($racks as $r) {
+        if ($temp === $r) {
+            $lis = query_rack($temp);
+			$temp1 = array();
+			foreach($results as $res) {
+				$temp1 = preg_split("/@@/", implode('',$res));
+			}
+			foreach ($temp1 as $value) {
+				if ($word == $value) {
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+	return false;
+    
+}
+
+/*function isValid($word) {
+    $arr = array();
+    foreach($results as $r) {
+        $arr = preg_split("/@@/", implode('',$r));
+    }
+    foreach ($arr as $a) {
+        if ($word == $a) {
+            return true;
+        }
+    }
+    return false;
+}*/
+
+$verb = $_SERVER["REQUEST_METHOD"];
+switch ($verb) {
+    case 'GET':    
+      echo json_encode(array("rack"=>$myrack, "subracks"=>$racks));
+      break;
+	case 'POST':    
+	 $answer = file_get_contents("php://input");	
+	 $correct = checkCombination($answer);
+	 // var_dump($answer);
+	//	echo file_get_contents("php://input");
+      //echo json_encode($correct);
+      break; 
+  };
+
+//echo json_encode(array("rack"=>$myrack, "subracks"=>$racks));
 
 
 
--- textwist.js
+++ textwist.js
@@ -2,7 +2,9 @@ var placeholders;
 var lettersEntered = [];
 var entries;
 var count = 0;
-var temp;
+let temp;
+var score = 0;
+
 
 
 function keyupHandler(event) {
@@ -11,12 +13,13 @@ function keyupHandler(event) {
   var letters = document.querySelectorAll(".letter");
   for (var i = 0; i < placeholders.length; i++) {
     if ((placeholders[i] == String.fromCharCode(letter)) && (lettersEntered.length < 7)) {
-      lettersEntered.push(letter);
+      lettersEntered.push(String.fromCharCode(letter));
       placeholders.splice(i, 1);
+      break;
     }
   }
   for (var i = 0; i < lettersEntered.length; i++) {
-    letters[i].innerText = String.fromCharCode(lettersEntered[i]);
+    letters[i].innerText = lettersEntered[i];
   }
 }
   
@@ -33,18 +36,36 @@ function newLetters() {
       if (this.status == 200) {
         var newRack = JSON.parse(this.response);
         placeholders = newRack["rack"].split('');
-        temp = placeholders;
-        entries = document.querySelectorAll(".rack");
+        var entries = document.querySelectorAll(".rack");
         for (var i = 0; i < placeholders.length; i++) {
           entries[i].innerText = placeholders[i];
         }
+        temp = newRack["rack"].split('');
         count = 1;
+        var letters = document.querySelectorAll(".letter");
+        for (var i = 0; i < letters.length; i++) {
+          letters[i].innerText = "_";
+        }
+        lettersEntered.length = 0;
       }
     };
     xhttp.open("GET", "backend.php");
     xhttp.send();
     
 }
+
+function twist() {
+  for (var i = 0; i < placeholders.length; i++) {
+    var randomIndex = Math.floor(Math.random() * i);
+    [placeholders[i], placeholders[randomIndex]] = [placeholders[randomIndex], placeholders[i]];
+  }
+  var entries = document.querySelectorAll(".rack");
+  for (var i = 0; i < placeholders.length; i++) {
+    entries[i].innerText = placeholders[i];
+  }
+}
+
+
 
 function clearGuess() {
   var letters = document.querySelectorAll(".letter");
@@ -53,7 +74,27 @@ function clearGuess() {
   
   }
   lettersEntered.length = 0;
-  placeholders = temp;
+  for (var i = 0; i < temp.length; i++) {
+    placeholders[i] = temp[i];
+  }
+  
+}
+
+function enter() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "backend.php");
+  var send = lettersEntered.join('');
+    xhttp.onload = function() {
+      if (this.status == 200) {
+        console.log(JSON.parse(this.response));
+        var valid = JSON.parse(this.response);
+        if (valid["answer"]) {
+          score += 10;
+        }
+        score = document.querySelector("#score");
+      }
+    };
+    xhttp.send(send);
 }
 
 /*function newLetters1() {
